48d9495e6d92d68b9096c1ae938f954fdfeed629
schujack-ctrl <86435255+jackrschumacher@users.noreply.github.com>
2022-02-18 19:50:48 -0600

    Setup Word Check Function

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from difflib import diff_bytes
 from random import random, randrange #Import random
 
 
-words = ["REALLY", "LITTLE", "SHOULD", "PLEASE", "PEOPLE", "THINGS", "BETTER"]
-randomWord = randrange(0, 5, 1)
-difficulty = 0
+words = ["REALLY", "LITTLE", "SHOULD", "PLEASE", "PEOPLE", "THINGS", "BETTER"] #List of Words
+randomWord = randrange(0, 5, 1)  #Range of Words 
+difficulty = 0 # Difficulty level-yet to be integrated
 
-print("---Python Wordle---")
+print("---Python Wordle---") #Print Title
 print("=====================================")
 print("The word has six letters and you have 5 chances to correctly guess the word")
+
+print("=====================================") # First Guess
+print("GUESS #1:")
+
+count = 0
+
+userInput = str(input("1: "))
+
+if userInput == randomWord:
+    print("Congratulations, you have selected the Correct word!")
+
+for i in userInput:
+    if userInput.# length
+        print(i)
+    
