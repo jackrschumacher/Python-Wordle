d384b28fc63f297e5e8e551b3a8e7a2bb119626b
schujack-ctrl <86435255+jackrschumacher@users.noreply.github.com>
2022-02-18 19:34:18 -0600

    Setup

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from random import random, randrange
+from difflib import diff_bytes
+from random import random, randrange #Import random
 
 
 words = ["REALLY", "LITTLE", "SHOULD", "PLEASE", "PEOPLE", "THINGS", "BETTER"]
 randomWord = randrange(0, 5, 1)
+difficulty = 0
 
-print(words, randomWord)
+print("---Python Wordle---")
+print("=====================================")
+print("The word has six letters and you have 5 chances to correctly guess the word")
